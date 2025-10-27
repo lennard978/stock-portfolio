9ab5939451e6e2fbfabf361af66f46dec911df5c
Lennard978 <shparavalo78nenad@gmail.com>
2025-10-27 17:36:32 +0100

    Deploy stock-portfolio PWA

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ export default function Header({ totalWealth, totalCost, totalProfit, totalDepos
   const totalPercent = totalCost ? ((totalProfit / totalCost) * 100).toFixed(2) : '0.00'
 
   return (
-    <header className="flex flex-row sm:flex-row sm:items-center justify-between py-4 transition-colors duration-300 space-y-3 sm:space-y-0">
-      {/* Left side: Portfolio summary */}
+    <header className="flex  sm:flex-row items-center justify-between py-4 transition-colors duration-300 sm:space-y-0">
+
+      {/* Left side: Total Wealth */}
       <div className="text-left">
         <div className="text-sm text-gray-500 dark:text-gray-400">Total Wealth</div>
         <div className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
           €{totalWealth.toFixed(2)}
         </div>
-
         <div className="text-sm">
           <span
             className={
@@ -28,14 +28,17 @@ export default function Header({ totalWealth, totalCost, totalProfit, totalDepos
         </div>
       </div>
 
-      {/* Right side: Deposit & Fees */}
-      <div className="text-right">
-        <div className="text-sm text-gray-500 dark:text-gray-400">Total Deposit</div>
+      {/* Middle: Total Deposit */}
+      <div className="text-center">
+        <div className="text-sm pt-0 text-gray-500 dark:text-gray-400">Total Deposit</div>
         <div className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
           €{totalDeposit.toFixed(2)}
         </div>
+      </div>
 
-        <div className="text-sm text-gray-500 dark:text-gray-400 mt-1">Total Fees</div>
+      {/* Right side: Total Fees */}
+      <div className="text-right">
+        <div className="text-sm text-gray-500 dark:text-gray-400">Total Fees</div>
         <div className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
           €{totalFees.toFixed(2)}
         </div>
